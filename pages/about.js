2e753329a0ca17a570376dde66c710ad327acdd9
isamu <isamumuraguchi.mail@gmail.com>
2025-02-05 08:02:36 +0900

    deploy

--- pages/about.js
+++ pages/about.js
@@ -34,7 +34,7 @@ export default function Home() {
             height={38}
             priority
           />
-          <p>index</p>
+          <p>about</p>
           <ol>
             <li>
               Get started by editing <code>pages/index.js</code>.
